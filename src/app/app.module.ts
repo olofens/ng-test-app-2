d70bf0bd1fec024126188c066cce735a84654de3
olofens <olofens@student.chalmers.se>
2020-05-08 15:09:21 +0200

    make work yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// our own library
+import {OeLibraryModule} from '../../oe-library/src/lib/oe-library.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    OeLibraryModule,
     BrowserModule,
     AppRoutingModule
   ],
